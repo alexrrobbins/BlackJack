41de64a5ff4c01ae7b65f2511740149cf2c3824f
Alex Robbins <alexrrobbins@gmail.com>
2019-02-17 16:24:51 -0500

    Removed pycache and added gitignore

--- Deck.py
+++ Deck.py
@@ -3,10 +3,13 @@ class Deck():
         self.cards = [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,
         9,9,9,9,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,'x','x','x','x']
 
+    def __str__(self):
+        return str(self.cards)
+
     def __len__(self):
         return len(self.cards)
 
-    def suffle(self):
+    def shuffle(self):
         pass
 
     def draw(self):
--- Deck_Test.py
+++ Deck_Test.py
@@ -2,3 +2,4 @@ from Deck import Deck
 
 deck1 = Deck()
 print(len(deck1))
+print(deck1)
