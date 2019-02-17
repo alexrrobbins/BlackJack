6376a2f69f4fa6ef82038007f2ee35ac0b7a5961
Alex Robbins <alexrrobbins@gmail.com>
2019-02-17 16:57:40 -0500

    Starting to test Player class

--- Player.py
+++ Player.py
@@ -1,9 +1,11 @@
 class Player():
-    def __init__(self, money, hand, amount=0):
+    def __init__(self, money, hand=[], amount=0, hand_value=0):
         self.money = money
         self.hand = hand
         self.amount = amount
+        self.hand_value = hand_value
 
+    #Money related methods
     def setAmount(self, bet_amount):
         if (self.money - bet_amount < 0):
             print("You do not have enough money for that bet!")
@@ -17,8 +19,20 @@ class Player():
     def loseGame(self):
         self.money -= self.amount
 
+    #Game related methods
     def initize_hand(self, card1, card2):
         self.hand = [card1, card2]
+        try:
+            self.hand_value = card1+card2
+        except:
+            ace = input("Do you want your ace to equal 1 or 11?")
+            if (card1.isdigit()):
+                card1 = int(ace)
+            elif (card2.isdigit()):
+                card2 = int(ace)
+            self.hand_value = card1+card2
+        finally:
+            return self.hand_value
 
     def stand(self):
         pass
---
+++ Player_Test.py
@@ -0,0 +1,5 @@
+import Player from Player
+
+player1 = Player(money=100.00)
+bet_amount = float(input("Enter bet amount: "))
+player1.setAmount(bet_amount)
