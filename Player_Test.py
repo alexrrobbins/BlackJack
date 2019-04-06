aa2125d61e7509dfb9d64e1b6f3922a66627f659
Alex Robbins <alexrrobbins@gmail.com>
2019-04-06 15:45:10 -0400

    Formalized the test script

--- Player_Test.py
+++ Player_Test.py
@@ -1,13 +1,19 @@
 from Player import Player
 from Ace import Ace
+import unittest
 
-ace = Ace()
-player1 = Player(money=100.00)
-player1.setAmount()
+class PlayerTest(unittest.TestCase):
+    def test_no_ace(self):
+        player1 = Player(money=100.00)
+        result = player1.initializeHand(10,2)
+        self.assertEqual(result,12)
 
-print("----------------------------------")
-print(player1.initializeHand(10,2))
+    def test_ace(self):
+        player2 = Player(money=300)
+        ace = Ace()
+        print("Please enter 1 when prompted")
+        result = player2.initializeHand(5,ace)
+        self.assertEqual(result,6)
 
-player2 = Player(money=300)
-print("----------------------------------")
-print(player2.initializeHand(5,ace)) #Returning 0 right now
+if __name__ == '__main__':
+    unittest.main()
